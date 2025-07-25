cde1c2f4983a14e38c67dd1bc0e58be0fa5c7bf9
cnagyei <cnagyei15@gmail.com>
2025-07-25 10:29:52 +0000

    Update

---
+++ Java_Full_Stack/src/Input.java
@@ -0,0 +1,22 @@
+import java.util.Scanner;
+
+public class Input {
+	public static void main(String[] args) {
+		System.out.println("Hello, World!");
+
+		Scanner sc = new Scanner(System.in);
+
+		// Capture only one token or word
+		String firstName = sc.next();
+		System.out.println(firstName);
+		String lastName = sc.next();
+
+		//Capture tokens on one line
+		String fullName = sc.nextLine();
+
+		System.out.println("Hello " + firstName + " " + lastName + "!");
+		System.out.println("Hi Everyone! My full name is " + fullName + "!");
+
+		sc.close();
+	}
+}
---
+++ Java_Full_Stack/src/Main.java
@@ -0,0 +1,38 @@
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class Main {
+    public static void main(String[] args) {
+        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
+        // to see how IntelliJ IDEA suggests fixing it.
+        System.out.printf("Hello and welcome!");
+
+        for (int i = 1; i <= 5; i++) {
+            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
+            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
+            System.out.println("i = " + i);
+        }
+        int numApples = 1_000_000;
+        char charOne = '1';
+        int numOne = 1;
+        String email = "cnagyei15@gmail.com";
+
+        System.out.println(numApples);
+        System.out.println(charOne);
+        System.out.println(numOne);
+        System.out.println(email);
+
+        System.out.print(108);   // printing a number
+        System.out.print('c');   // printing a character that represents a letter
+        System.out.print("Q");   // printing a string
+        System.out.println('3'); // printing a character that represents a digit
+        System.out.print('3'); // printing a character that represents a digit
+        System.out.println('3'); // printing a character that represents a digit
+        System.out.print(22);
+        System.out.print('E');
+        System.out.print(8);
+        System.out.println('1');
+
+        var number = 20;
+        System.out.println(number);
+    }
+}
