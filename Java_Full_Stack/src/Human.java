69c9c3a6dc5aef5612ccfcd6e63c9be182c04359
cnagyei <cnagyei15@gmail.com>
2025-08-05 19:38:46 +0000

    Reorganize

---
+++ Java_Full_Stack/src/Human.java
@@ -0,0 +1,40 @@
+public class Human {
+    String name;
+    int age;
+
+    public static void averageWorking() {
+        System.out.println("An average human works 40 hours per week.");
+    }
+
+    public void work() {
+        System.out.println(this.name + " loves working!");
+    }
+
+    public void workTogetherWith (Human other) {
+        System.out.println(this.name + " loves working with " + other.name);
+    }
+}
+
+class MainClass {
+    public static void main(String[] args) {
+        // Static method available
+        Human.averageWorking(); // An average human works 40 hours per week.
+
+        // Create new Human Peter
+        Human peter = new Human();
+        peter.name = "Peter";
+        peter.age = 24;
+        peter.work();
+
+        // Create new Human Alice
+        Human alice = new Human();
+        alice.name = "Alice";
+        alice.age = 30;
+        alice.work();
+
+        peter.workTogetherWith(alice);
+        alice.workTogetherWith(peter);
+
+    }
+}
+
