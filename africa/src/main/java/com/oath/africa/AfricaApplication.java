b5e4b2af971ed1b79c78d49469c28057bfb20be3
cnagyei <cnagyei15@gmail.com>
2025-11-13 15:46:10 +0000

    New imports

--- africa/src/main/java/com/oath/africa/AfricaApplication.java
+++ africa/src/main/java/com/oath/africa/AfricaApplication.java
@@ -2,6 +2,11 @@ package com.oath.africa;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
+import org.springframework.stereotype.Component;
 
 @SpringBootApplication
 public class AfricaApplication {
